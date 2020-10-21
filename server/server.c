f7dd292350a39984ac9fc2223407c4fb9dee44bc
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-22 01:34:11 +0330

    Add register packet's process to server

--- server/server.c
+++ server/server.c
@@ -15,8 +15,9 @@ int main() {
     int sockfd;
     char buffer[MAXLINE]; 
     char *hello = "Hello from server"; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
+    struct sockaddr_in servaddr, cliaddr;
+    file *data = (file*)malloc((sizeof(char))*(MAXFILENUM+MAXIPSIZE+MAXPORTSIZE);
+    printf("size of data elements :%lu\n",sizeof(file));
     // Creating socket file descriptor 
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
@@ -39,20 +40,48 @@ int main() {
       
     int  n; 
     unsigned int len;
-    char str[54];
-  
+    int level=0;
+    int start=1;
+    int current_size = 0;
+
     len = sizeof(cliaddr);  //len is value/resuslt 
     while(sockfd > 0){
         n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
                     MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
                     &len);
-        inet_ntop(AF_INET, &(cliaddr.sin_addr), str, INET_ADDRSTRLEN);
-        printf("client port and ip %d , %d\n",ntohl(cliaddr.sin_port),strlen(str));
-
+        // inet_ntop(AF_INET, &(cliaddr.sin_addr), str, INET_ADDRSTRLEN);
         buffer[n] = '\0'; 
         switch (buffer[0])
         {
             case 'R':
+                printf("received a register request.\n");
+                level=0;
+                start=1;
+                for (int i=0;i<strlen(buffer);i++)
+                {
+                    if(buffer[i] == '#')
+                    {
+                        if(level == 0){
+                            data[current_size].filename = (char*)malloc(sizeof(char)*MAXNAMESIZE);
+                            strncpy(data[current_size].filename, buffer+start,i -start);
+                            level ++;
+                        }
+                        else if(level == 1)
+                        {
+                            data[current_size].fileip = (char*)malloc(sizeof(char)*MAXIPSIZE);
+                            strncpy(data[current_size].fileip, buffer+start,i -start);
+                            level ++;
+                        }
+                        else{
+                            data[current_size].fileport = (char*)malloc(sizeof(char)*MAXPORTSIZE);
+                            strncpy(data[current_size].fileport, buffer+start,i -start);
+                            break;
+                        }
+
+                        start = i + 1;
+                    }
+                }
+                current_size++;
                 break;
             case 'S':
                 break;
@@ -66,7 +95,8 @@ int main() {
                 break;
         }
             
-        printf("Client : %s\n", buffer); 
+        printf("Client : %s\n", buffer);
+        printf("data: %s, %s ,%s\n",data[0].filename,data[0].fileip,data[0].fileport);
         sendto(sockfd, (const char *)hello, strlen(hello),  
             0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr)); 
         printf("Hello message sent.\n");
