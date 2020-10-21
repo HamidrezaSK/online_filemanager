eedc389a57828c608a5de7c570d54361358133f7
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-22 01:31:19 +0330

    Add find my ip function

--- client/client_func.c
+++ client/client_func.c
@@ -6,23 +6,39 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h>
+#include <ifaddrs.h>
 #include "client_func.h"
 
+char* get_my_ip()
+{
+    struct ifaddrs *id;
+    char *str = (char *) malloc(15);
+    if(getifaddrs(&id)!=0)
+    {
+        printf("cant find my ip\n");
+    }
+    inet_ntop(AF_INET, &(id->ifa_addr), str, INET_ADDRSTRLEN);
+    return str;
+}
 
-char* create_messaage(char* message,char* ip)
+char* create_register_messaage(char* message)
 {
     char *str;
     str = (char *) malloc(101);
-    strcpy(str, message);
+    strcpy(str, "R");
+    strcat(str, message);
+    char *sip = get_my_ip();
     char sport[5];
     sprintf(sport,"%d",P2PPORT);
     strcat(str,"#");
-    strcat(str,ip);
+    strcat(str,sip);
     strcat(str,"#");
     strcat(str,sport);
+    strcat(str,"#");
     printf("in create_message: %s\n",str);
     return str;
 }
+
 
 
 
