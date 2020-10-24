519d781763d729912ae8d4663f27b789b924f118
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-24 03:40:50 +0330

    Add register and delete process to server

--- server/server.c
+++ server/server.c
@@ -8,18 +8,25 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 #include "server.h"
+#include "server_func.h"
 
-  
+
+file *data = NULL;
+int current_size = 0;
+
 // Driver code 
 int main() { 
     int sockfd;
-    char buffer[MAXLINE]; 
-    char *hello = "Hello from server"; 
+    char buffer[MAXLINE];
+    char *message;
     struct sockaddr_in servaddr, cliaddr;
-    file *data = (file*)malloc((sizeof(char))*(MAXFILENUM+MAXIPSIZE+MAXPORTSIZE);
-    printf("size of data elements :%lu\n",sizeof(file));
+    if (data == NULL)
+    {
+        data = (file*) malloc(sizeof(char)*(MAXFILENUM+MAXIPSIZE+MAXPORTSIZE));
+    }
+   
     // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
@@ -40,52 +47,32 @@ int main() {
       
     int  n; 
     unsigned int len;
-    int level=0;
-    int start=1;
-    int current_size = 0;
-
     len = sizeof(cliaddr);  //len is value/resuslt 
+
     while(sockfd > 0){
         n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
                     MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
                     &len);
-        // inet_ntop(AF_INET, &(cliaddr.sin_addr), str, INET_ADDRSTRLEN);
         buffer[n] = '\0'; 
         switch (buffer[0])
         {
             case 'R':
                 printf("received a register request.\n");
-                level=0;
-                start=1;
-                for (int i=0;i<strlen(buffer);i++)
+                if(process_register_req(buffer)==0)
+                    message= create_ack_response();
+                else
                 {
-                    if(buffer[i] == '#')
-                    {
-                        if(level == 0){
-                            data[current_size].filename = (char*)malloc(sizeof(char)*MAXNAMESIZE);
-                            strncpy(data[current_size].filename, buffer+start,i -start);
-                            level ++;
-                        }
-                        else if(level == 1)
-                        {
-                            data[current_size].fileip = (char*)malloc(sizeof(char)*MAXIPSIZE);
-                            strncpy(data[current_size].fileip, buffer+start,i -start);
-                            level ++;
-                        }
-                        else{
-                            data[current_size].fileport = (char*)malloc(sizeof(char)*MAXPORTSIZE);
-                            strncpy(data[current_size].fileport, buffer+start,i -start);
-                            break;
-                        }
-
-                        start = i + 1;
-                    }
+                    printf("could not process register request");
+                    message = create_error_response(400);
                 }
-                current_size++;
                 break;
             case 'S':
                 break;
             case 'U':
+                if(process_delete_req(buffer)==0)
+                    message=create_ack_response();
+                else
+                    message = create_error_response(404);
                 break;
             case 'O':
                 break;
@@ -94,12 +81,16 @@ int main() {
             case 'E':
                 break;
         }
+        
             
         printf("Client : %s\n", buffer);
         printf("data: %s, %s ,%s\n",data[0].filename,data[0].fileip,data[0].fileport);
-        sendto(sockfd, (const char *)hello, strlen(hello),  
-            0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr)); 
-        printf("Hello message sent.\n");
+        printf("current_size: %d\n",current_size);
+        printf("message: %s\n",message);
+        sendto(sockfd, (const char *)message, strlen(message),  
+            0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
+        free(message); 
+        printf("response message sent.\n");
     }
       
     return 0; 
--- server/server.h
+++ server/server.h
@@ -4,6 +4,10 @@
 #define MAXPORTSIZE 4
 #define MAXIPSIZE 15
 #define MAXNAMESIZE 79
+#define ERROR_404 "NOT FOUND"
+#define ERROR_400 "BAD REQUEST"
+
+
 
 
 typedef struct {
@@ -11,3 +15,6 @@ typedef struct {
     char *fileport;
     char *fileip;
 } file;
+
+extern file *data;
+extern int current_size;
