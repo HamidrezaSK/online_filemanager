31c4244582d55f7f497d65d906eace0ab753e214
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-25 02:24:09 +0330

    Add hashtable to serverfunctions and also send and receive process and get all files process

--- server/server_func.c
+++ server/server_func.c
@@ -8,39 +8,43 @@
 #include <netinet/in.h> 
 #include "server_func.h"
 #include "server.h"
+#include "hashtable.h"
 
 int process_register_req(char* message)
 {
     int level=0;
     int start=1;
+    int status;
+    char* filename= (char*)malloc(sizeof(char)*MAXNAMESIZE);
+    char* fileip= (char*)malloc(sizeof(char)*MAXIPSIZE);
+    char* fileport= (char*)malloc(sizeof(char)*MAXPORTSIZE);
     for (int i=0;i<strlen(message);i++)
     {
         if(message[i] == '#')
         {
             if(level == 0){
-                data[current_size].filename = (char*)malloc(sizeof(char)*MAXNAMESIZE);
-                strncpy(data[current_size].filename, message+start,i -start);
+                strncpy(filename, message+start,i -start);
                 level ++;
             }
             else if(level == 1)
             {
-                data[current_size].fileip = (char*)malloc(sizeof(char)*MAXIPSIZE);
-                strncpy(data[current_size].fileip, message+start,i -start);
+                strncpy(fileip, message+start,i -start);
                 level ++;
             }
             else{
-                data[current_size].fileport = (char*)malloc(sizeof(char)*MAXPORTSIZE);
-                strncpy(data[current_size].fileport, message+start,i -start);
+                strncpy(fileport, message+start,i -start);
                 break;
             }
-
             start = i + 1;
         }
     }
-    current_size++;
+    status = install(filename,fileip,fileport);
+    free(filename);
+    free(fileip);
+    free(fileport);
     if(level == 2)
-        return 0;
-    return -1;
+        return status;
+    return 400;
 }
 
 int process_delete_req(char* message)
@@ -50,7 +54,7 @@ int process_delete_req(char* message)
     char* fileport= (char*)malloc(sizeof(char)*MAXPORTSIZE);
     int level=0;
     int start=1;
-    int item_index = -1;
+    int status;
     for (int i=0;i<strlen(message);i++)
     {
         if(message[i] == '#')
@@ -72,30 +76,13 @@ int process_delete_req(char* message)
             start = i + 1;
         }
     }
-    for(int i = 0;i<current_size-1;i++)
-    {
-        if(data[i].filename == filename && data[i].fileip == fileip && data[i].fileport == fileport)
-        {
-            item_index = i;
-            continue;
-        }
-        if(item_index!=-1)
-        {
-            data[i].filename = data[i+1].filename;
-            data[i].fileip = data[i+1].fileip;
-            data[i].fileport = data[i+1].fileport;
-        }
-    }
-    if(item_index == -1)
-        return -1;
-    current_size --;
-    free(data[current_size].filename);
-    free(data[current_size].fileip);
-    free(data[current_size].fileport);
+    status = del(filename,fileip,fileport);
     free(filename);
     free(fileip);
     free(fileport);
-    return 0;   
+    if(level == 2)
+        return status;
+    return 400;  
 }
 
 char* create_ack_response(void)
@@ -114,8 +101,94 @@ char* create_error_response(int code)
     {
         case 404:
             strcat(str, ERROR_404);
+            break;
         case 400:
             strcat(str, ERROR_400);
+            break;
+        case 406:
+            strcat(str, ERROR_406);
+            break;
+        case 500:
+            strcat(str, ERROR_500);
+            break;
+        case 512:
+            strcat(str, ERROR_512);
+            break;
     }
     return str;
+}
+
+void send_error_response(int status,int sockfd,struct sockaddr_in cliaddr)
+{
+    char * message = create_error_response(status);
+    sendto(sockfd, (const char *)message, strlen(message),  
+        0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
+    free(message);
+    return;
+}
+
+void send_ack_response(int sockfd,struct sockaddr_in cliaddr)
+{
+    char * message = create_ack_response();
+    sendto(sockfd, (const char *)message, strlen(message),  
+        0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
+    free(message);
+    return;
+}
+char* create_allfiles_response(char* message,int islast)
+{
+    char *str;
+    str = (char *) malloc(101);
+    if(islast == 0)
+        strcpy(str, "O");
+    else
+        strcpy(str, "L");
+    strcat(str, message);
+    return str;
+}
+
+void send_allfiles_response(int sockfd,struct sockaddr_in cliaddr)
+{
+    int remaining_files = 0;
+    char* message;
+    int timeout;
+    char buffer[MAXLINE];
+    unsigned int len;
+    len = sizeof(cliaddr);
+    int i;
+    for(i = 0;i<HASHSIZE;i++)
+        if(hashtab[i]!=NULL)
+            remaining_files++;
+    i = 0;
+    while(remaining_files>0)
+    {
+        timeout = 0;
+        if(hashtab[i]!=NULL)
+        {
+            if(remaining_files>1)
+            {
+                message = create_allfiles_response(hashtab[i]->name,0);
+            }
+            else
+                message = create_allfiles_response(hashtab[i]->name,1);
+            remaining_files--;
+        }
+        while(timeout>3)
+        {
+            sendto(sockfd, (const char *)message, strlen(message),  
+                0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
+            recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                    &len);
+            if(buffer == "A")
+                break;
+            else
+                timeout++;
+        
+        }
+        free(message);
+        i++;
+
+    }
+    return;
 }
--- server/server_func.h
+++ server/server_func.h
@@ -2,3 +2,8 @@ int process_register_req(char*);
 int process_delete_req(char*);
 char* create_ack_response(void);
 char* create_error_response(int);
+void send_error_response(int,int,struct sockaddr_in);
+void send_ack_response(int,struct sockaddr_in);
+void send_allfiles_response(int,struct sockaddr_in);
+
+
