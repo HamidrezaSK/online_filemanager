629400b911fc06518d1c4b8682af97491ff02083
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-25 02:26:20 +0330

    Add hashtable to server and move send and recieve processes to server_func

--- server/server.c
+++ server/server.c
@@ -9,29 +9,26 @@
 #include <netinet/in.h> 
 #include "server.h"
 #include "server_func.h"
+#include "hashtable.h"
 
 
-file *data = NULL;
-int current_size = 0;
+
+
+
 
 // Driver code 
 int main() { 
     int sockfd;
     char buffer[MAXLINE];
-    char *message;
     struct sockaddr_in servaddr, cliaddr;
-    if (data == NULL)
-    {
-        data = (file*) malloc(sizeof(char)*(MAXFILENUM+MAXIPSIZE+MAXPORTSIZE));
-    }
+    int status;
    
     // Creating socket file descriptor 
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
-      
-      
+
     // Filling server information 
     servaddr.sin_family    = AF_INET; // IPv4 
     servaddr.sin_addr.s_addr = INADDR_ANY; 
@@ -58,39 +55,41 @@ int main() {
         {
             case 'R':
                 printf("received a register request.\n");
-                if(process_register_req(buffer)==0)
-                    message= create_ack_response();
+                status = process_register_req(buffer);
+                if(status==0)
+                    send_ack_response(sockfd,cliaddr);
                 else
                 {
-                    printf("could not process register request");
-                    message = create_error_response(400);
+                    printf("could not process register request.\n");
+                    send_error_response(status,sockfd,cliaddr);
                 }
                 break;
             case 'S':
                 break;
             case 'U':
-                if(process_delete_req(buffer)==0)
-                    message=create_ack_response();
+                status = process_delete_req(buffer);
+                printf("this is the status : %d\n",status);
+                if(status==0)
+                    send_ack_response(sockfd,cliaddr);
                 else
-                    message = create_error_response(404);
+                    send_error_response(status,sockfd,cliaddr);
                 break;
             case 'O':
+                send_allfiles_response(sockfd,cliaddr);
                 break;
             case 'A':
                 break;
             case 'E':
                 break;
         }
-        
-            
-        printf("Client : %s\n", buffer);
-        printf("data: %s, %s ,%s\n",data[0].filename,data[0].fileip,data[0].fileport);
-        printf("current_size: %d\n",current_size);
-        printf("message: %s\n",message);
-        sendto(sockfd, (const char *)message, strlen(message),  
-            0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
-        free(message); 
-        printf("response message sent.\n");
+        struct nlist *np=lookup("install.exe");
+        printf("files count : %d , filename: %s ,\n", np->owner_count,np->name);
+        for(int i =0 ; i<np->owner_count;i++)
+        {
+            printf("owner : %s , %s \n",np->owners[i].ip,np->owners[i].port);
+        }
+        // sendto(sockfd, (const char *)message, strlen(message),  
+        //     0, (const struct sockaddr *) &cliaddr, sizeof(cliaddr));
     }
       
     return 0; 
