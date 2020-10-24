e011a8ec26d125f149371ceeaaeb05d81113d385
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-25 02:27:39 +0330

    Add error code to hashtable functions and fix del function

--- server/hashtable.c
+++ server/hashtable.c
@@ -4,7 +4,7 @@
 
 
 
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+struct nlist *hashtab[HASHSIZE]; /* pointer table */
 
 /* hash: form hash value for string s */
 unsigned hash(char *s)
@@ -27,67 +27,105 @@ struct nlist *lookup(char *s)
 
 // char *strdup(char *);
 /* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *ip , char *port)
+int install(char *name, char *ip , char *port)
 {
     struct nlist *np;
     unsigned hashval;
     if ((np = lookup(name)) == NULL) { /* not found */
         np = (struct nlist *) malloc(sizeof(*np));
         if (np == NULL || (np->name = strdup(name)) == NULL)
-          return NULL;
+          return 500;
         hashval = hash(name);
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
         np->owner_count = 0;
         np->owners = (owner*) malloc(sizeof(owner)*(20));//MAXOWNERSIZE = 20;
     }
-    //  else{ /* already there */
-        
+     else{ /* already there */
+        for(int i = 0;i<np->owner_count;i++)
+        {
+            if(np->owners[i].ip==ip && np->owners[i].port==port)
+                return 406;
+        }
     //         np->owners[np->owner_count] = {port, ip};
     //         np->owner_count++;
-    //     }
+        }
     if(np->owner_count >= 20)
-        return NULL;
+        return 512;
     owner temp = {port,ip};
     np->owners[np->owner_count] =temp;
     np->owner_count++;
-    return np;
+    return 0;
 }
 
 int del(char *name, char *ip , char *port)
 {
     struct nlist *np;
+    int item_index = -1;
     if ((np = lookup(name)) != NULL) /* found */
     {
+        printf("found it,%d\n",np->owner_count);
+        for(int i = 0;i<np->owner_count;i++)
+        {
+            if(item_index == -1&&np->owners[i].port == port && np->owners[i].ip == ip)
+            {
+                item_index = i;
+                continue;
+            }
+            if(item_index!=-1&&i<np->owner_count-1)
+            {
+                np->owners[i].ip= np->owners[i+1].ip;
+                np->owners[i].port= np->owners[i+1].port;
+            }
+        }
         if(np->owner_count>0)
             np->owner_count --;
     }
     else{ /* not found */
         return 404;
     }
+    if(item_index == -1)
+     return 404;
     if(np->owner_count==0)
+    {
         free(np->owners);
+        unsigned hashval = hash(name);
+        hashtab[hashval] = NULL;
+    }
     return 0;
 }
 
 
 // int main()
 // {
-//     struct nlist *np = install("fuck.exe","127.0.0.1","3443");
-//     np = install("df.exe","127.0.0.1","3443");
-//     np = install("fuck.exe","127.0.0.1","3543");
+//     int err = install("fuck.exe","127.0.0.1","3443");
+//     printf("err is : %d\n",err);
+
+//      err = install("df.exe","127.0.0.1","3443");
+//     printf("err is : %d\n",err);
+
+//     err = install("fuck.exe","127.0.0.1","3543");
+//     printf("err is : %d\n",err);
+//     err = install("fuck.exe","127.0.0.1","3543");
+//     printf("err is : %d\n",err);
+
 //     // np = install("suck","mmd");
 //     // np = install("khaak","akbar");
-//     np = lookup("fuck.exe");
+//     struct nlist *np = lookup("fuck.exe");
 //     printf("owners of fuck.exe are: \n");
 //     for (int i = 0;i<np->owner_count;i++)
 //         printf("ip : %s , port : %s\n",np->owners[i].ip,np->owners[i].port);
 //     // printf("khaak was : %s , %s\n",np->name,np->defn);
 //     int siz = 0;
+//     err = del("fucka.exe","127.0.0.1","353");
+//     printf("err is : %d\n",err);
 //     for(int i = 0;i<HASHSIZE;i++)
 //     {
 //         if(hashtab[i]!=NULL)
+//         {
 //             siz++;
+//             printf("%s\n",hashtab[i]->name);
+//         }
 //     }
 //     printf("hashtab size is : %d\n",siz);
 
