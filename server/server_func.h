db863031a854f3da1f534974013a92d650862448
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-28 16:21:02 +0330

    Update server_func header file for search resp

--- server/server_func.h
+++ server/server_func.h
@@ -5,5 +5,7 @@ char* create_error_response(int);
 void send_error_response(int,int,struct sockaddr_in);
 void send_ack_response(int,struct sockaddr_in);
 void send_allfiles_response(int,struct sockaddr_in);
+void send_search_response(char*,int,struct sockaddr_in);
+
 
 
