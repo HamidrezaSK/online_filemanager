b6b0544fc8d056ba20223822fc9eddbf9a884ba7
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-29 06:53:25 +0330

    Add get my ip to client function headerfile

--- client/client_func.h
+++ client/client_func.h
@@ -1,8 +1,9 @@
-void send_register_req(char*,int,struct sockaddr_in);
-void send_search_req(char*,int,struct sockaddr_in);
-void send_delete_req(char*,int,struct sockaddr_in);
-void send_getall_req(int,struct sockaddr_in);
-
+void  send_register_req(char*,int,struct sockaddr_in);
+int   send_search_req(char*,int,struct sockaddr_in);
+void  send_delete_req(char*,int,struct sockaddr_in);
+void  send_getall_req(int,struct sockaddr_in);
+void  print_all_serverfiles(void);
+char* get_my_ip(void);
 
 
 
