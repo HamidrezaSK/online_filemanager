8f17f03afba32b982ba6c2e2a1999c47e2017732
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-29 06:54:05 +0330

    Add network interface to client header file

--- client/client.h
+++ client/client.h
@@ -1,24 +1,21 @@
 #define P2PPORT 7656
 #define PORT    8080 
 #define MAXLINE 101
+#define MAXNAMESIZE 79
 #define MAXFILENUM 50
 #define SERVERMAXFILENUM 50
+#define MYINTERFACE "en0"
 
+// typedef struct {
+//     char *name; 
+//     int owner_count;
+//     owner *owners;
+// } file;
 
 typedef struct {
     char *name; 
-    int owner_count;
-    owner *owners;
 } file;
 
-typedef struct {
-    char *name; 
-} local_file;
-
-typedef struct {
-    char *port;
-    char *ip;
-} owner;
 
 extern  file *myfiles[MAXFILENUM];
 extern  file *serverfiles[SERVERMAXFILENUM];
