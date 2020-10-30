b00bd6e9273644813ffc1b36abe4a1e84e60a804
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-30 16:35:17 +0330

    Add functions and structs to peer headerfile

--- client/peer.h
+++ client/peer.h
@@ -1,2 +1,15 @@
 #define MAXLINE 101
 #define MAXNAMESIZE 79
+
+typedef struct {
+    char *name;
+    char *ip; 
+    char *port; 
+} request;
+
+typedef struct {
+    int sockfd; 
+} myarg_t;
+
+void client_setup(char* message);
+void * server_setup(void);
