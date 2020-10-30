935fdc2bef28a18ccbb320e79f08a201e80fe816
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-30 16:32:18 +0330

    Add file constants to client header file

--- client/client.h
+++ client/client.h
@@ -1,16 +1,14 @@
 #define P2PPORT 7656
 #define PORT    8080 
 #define MAXLINE 101
+#define MAXPORTSIZE 4
+#define MAXIPSIZE 15
 #define MAXNAMESIZE 79
+#define MAX_THREADS 79
 #define MAXFILENUM 50
 #define SERVERMAXFILENUM 50
 #define MYINTERFACE "en0"
 
-// typedef struct {
-//     char *name; 
-//     int owner_count;
-//     owner *owners;
-// } file;
 
 typedef struct {
     char *name; 
