1c7bbc754a4236b6abc5a01a49cc050329fb5f92
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-10-30 16:34:00 +0330

    Add add and delete from myfiles functions to headerfile

--- client/client_func.h
+++ client/client_func.h
@@ -4,6 +4,9 @@ void  send_delete_req(char*,int,struct sockaddr_in);
 void  send_getall_req(int,struct sockaddr_in);
 void  print_all_serverfiles(void);
 char* get_my_ip(void);
+void add_to_myfiles(char*);
+void delete_from_myfiles(char*);
+
 
 
 
